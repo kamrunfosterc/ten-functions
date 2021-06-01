1782bb4b98d8591d5e3590cccf3e0f11deabf052
Kamrun Foster <kamrun.foster4@gmail.com>
2021-06-01 16:30:51 -0500

    updated not

--- solutions.js
+++ solutions.js
@@ -9,10 +9,7 @@
  * If you don't know what an iffe is, don't worry about it :)
  *
  * This file is for defining the following functions. See README.md for more detail.
- * isTrue CHECK
- * isFalse CHECK
  * not
- * addOne CHECK
  * isEven
  * isIdentical
  * isEqual
@@ -20,50 +17,28 @@
  * and
  * concat
  */
-// isTrue function
-function isTrue(value){
-    return value === true;
-}
-// isFalse function
-function isFalse(value){
-    return value === false;
-}
 // not function
 function not(value){
-    return isTrue(value) === "";
+    switch
+    return isBoolean(!value);
 }
-//addOne function
-function addOne(value){
-    return Number(value) + 1;
-}
+
 //isEven function
 function isEven(value){
     //value = value % 2;
     return (value % 2 == true);
 }
-//isEqual function
-function isEqual(){
-    /* should return a boolean value
-should be a defined function
-should return true when passed 2 and "2" as inputs since they are equal in value
-should return false when passed "java" and "javascript" as inputs, since they are not equal in value
-should return false when passed -4 and 5 as inputs
-should return true when passed "3.6" and 3.6 as inputs, since they have the same value
-should return true when passed true and 1, since == performs type coersion
-should return true when passed 0 and false, since == performs type coersion*/
-}
+
 //or function
 function or(value){
-    if (true || false){
+    if (value = true || false){
         return true;
     }
 }
 
-//and function
-function and(){
-    if (true && true){
-        return
-    }
+//and function NO
+function isBoolean(input){
+    return typeof input == 'boolean';
     /* should return a boolean when passed two boolean values
 should return "world" when passed the inputs "hello" and "world" in that order
 should return false when passed true and false as the inputs
@@ -86,4 +61,20 @@ should be a defined function*/
 //isIdentical function
 function isIdentical(){
 
+}
+// isTrue function DONE
+function isTrue(value){
+    return value === true;
+}
+// isFalse function DONE
+function isFalse(value){
+    return value === false;
+}
+//addOne function DONE
+function addOne(value){
+    return Number(value) + 1;
+}
+//isEqual function DONE
+function isEqual(value1, value2){
+    return value1==value2
 }
